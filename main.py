e04c1e63ce8b1fe35c51f90ea0f71a31fa74efe4
findryDev <72649730+findryDev@users.noreply.github.com>
2020-11-17 10:28:22 +0100

    add time control and improve loger
    added time control and add loger time counter

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from requestData import requestPlotsData
+import timeControl
 import plots
 import htmlLinux
 import logging
+from time import sleep
 
 # create a custom logger
+loggerCounter = logging.getLogger('counter')
 loggerMain = logging.getLogger(__name__)
 loggerMain.setLevel(logging.DEBUG)
+loggerCounter.setLevel(logging.DEBUG)
 # create handlers
 print_handler = logging.StreamHandler()
-file_handler = logging.FileHandler('mainfile.log')
-print_handler.setLevel(logging.INFO)
-file_handler.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('mainFile.log')
 # create formatters and add it to handlers
 print_format = logging.Formatter('%(asctime)s - %(message)s')
 print_handler.setFormatter(print_format)
@@ -21,8 +23,7 @@ file_handler.setFormatter(file_format)
 # add handler to the logger
 loggerMain.addHandler(print_handler)
 loggerMain.addHandler(file_handler)
-
-
+loggerCounter.addHandler(print_handler)
 
 
 if __name__ == "__main__":
@@ -31,3 +32,7 @@ if __name__ == "__main__":
     loggerMain.debug('Finished create plots')
     htmlLinux.htmlMaker()
     loggerMain.debug('End scripts')
+    waitSeconds = timeControl.waitTo(8)
+    for i in range(waitSeconds, 0, -1):
+        loggerCounter.info(f'Wait {i}')
+        sleep(1)
--- timeControl.py
+++ timeControl.py
@@ -1,4 +1,9 @@
 import datetime as dt
 
-i = dt.datetime(hour=8, minute=0, second=0, year=dt.datetime.now().year, month=dt.datetime.now().month, day=dt.datetime.now().day) + dt.timedelta(days=1)
-print(int((i - dt.datetime.now()).total_seconds()))
+
+def waitTo(hour: int):
+    i = dt.datetime(hour=hour, minute=0, second=0,
+                    year=dt.datetime.now().year,
+                    month=dt.datetime.now().month,
+                    day=dt.datetime.now().day) + dt.timedelta(days=1)
+    return (int((i - dt.datetime.now()).total_seconds()))
