dc36747b25480d624527d3becff60bb5a6f56c7a
findryDev <72649730+findryDev@users.noreply.github.com>
2020-11-24 10:54:58 +0100

    delete history folder

--- .history/requestData_20201101170548.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-import pandas as pd
-
-url = 'https://covid2019-api.herokuapp.com/timeseries/confirmed'
-
-r = requests.get(url)
-print(r.text)
--- .history/requestData_20201101171319.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-import pandas as pd
-
-url = 'https://covid2019-api.herokuapp.com/timeseries/confirmed'
-
-r = requests.get(url)
-j = json.loads(r.text)
-print(type(j))
