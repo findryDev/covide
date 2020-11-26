19862ad27925ab72fc08b46a06e2a245aee59071
findryDev <72649730+findryDev@users.noreply.github.com>
2020-11-26 11:52:54 +0100

    correcting time zone

--- cov/timeControl.py
+++ cov/timeControl.py
@@ -1,9 +1,12 @@
 import datetime as dt
+import pytz
+
+IST = pytz.timezone('Europe/Warsaw')
 
 
 def waitTo(hour: int):
     i = dt.datetime(hour=hour, minute=0, second=0,
-                    year=dt.datetime.now().year,
-                    month=dt.datetime.now().month,
-                    day=dt.datetime.now().day) + dt.timedelta(days=1)
+                    year=dt.datetime.now(IST).year,
+                    month=dt.datetime.now(IST).month,
+                    day=dt.datetime.now(IST).day) + dt.timedelta(days=1)
     return (int((i - dt.datetime.now()).total_seconds()))
