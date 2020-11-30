89ea34dedf4b9d1a1480c7bf8155af6f29461278
findryDev <72649730+findryDev@users.noreply.github.com>
2020-11-30 04:50:40 +0100

    working on flags requests

--- testScript.py
+++ testScript.py
@@ -7,16 +7,19 @@ r = requests.get('https://covid2019-api.herokuapp.com/countries')
 j = json.loads(r.text)
 
 countries = j["countries"]
+countries = [country.replace('_', ' ') for country in countries]
 
 r = requests.get('https://flagcdn.com/en/codes.json')
 countryISO = json.loads(r.text)
 countryISO = {value: key for key, value in countryISO.items()}
+restCountry = []
 
-for key in countryISO:
-    if key in countries:
-        print(f'https://flagcdn.com/256x192/{countryISO[key]}.png')
-        r = requests.get(f'https://flagcdn.com/256x192/{countryISO[key]}.png')
-        with open(f'flags/{key}.png', 'wb') as f:
+for country in countries:
+    if country in countryISO.keys():
+        r = requests.get(f'https://flagcdn.com/256x192/{countryISO[country]}.png')
+        with open(f'flags/{country}.png', 'wb') as f:
             f.write(r.content)
+    else:
+        restCountry.append(country)
 
-cov.htmlLinux.htmlMaker()
+print(restCountry)
