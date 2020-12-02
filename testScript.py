9ff69dabdde504165e9db2a013634dbb46918e4e
findryDev <72649730+findryDev@users.noreply.github.com>
2020-12-02 04:49:23 +0100

    flag.py working

--- testScript.py
+++ testScript.py
@@ -8,18 +8,27 @@ j = json.loads(r.text)
 
 countries = j["countries"]
 countries = [country.replace('_', ' ') for country in countries]
+countries = [country.replace('US', 'United States') for country in countries]
+
 
 r = requests.get('https://flagcdn.com/en/codes.json')
 countryISO = json.loads(r.text)
 countryISO = {value: key for key, value in countryISO.items()}
-restCountry = []
+notCountry = []
 
 for country in countries:
-    if country in countryISO.keys():
-        r = requests.get(f'https://flagcdn.com/256x192/{countryISO[country]}.png')
-        with open(f'flags/{country}.png', 'wb') as f:
-            f.write(r.content)
-    else:
-        restCountry.append(country)
+    try:
+        if country in countryISO.keys():
+            r = requests.get(f'https://flagcdn.com/256x192/{countryISO[country]}.png')
+            with open(f'flags/{country}.png', 'wb') as f:
+                f.write(r.content)
+        else:
+            notCountry.append(country)
+    except Exception as e:
+        print(e)
+        notCountry.append(country)
+        continue
 
-print(restCountry)
+
+print(notCountry)
+print(countryISO)
